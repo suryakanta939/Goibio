e2518f839302c77770bde1d7fcc6d840b29c93ca
suryakanta939 <suryakanta939@gmail.com>
2017-09-26 19:40:57 +0530

    checking

---
+++ myibibo/src/test/java/com/goibibo/commonClasses/AndroidKeyboardFunctions.java
@@ -0,0 +1,15 @@
+package com.goibibo.commonClasses;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.android.AndroidKeyCode;
+
+public class AndroidKeyboardFunctions {
+	
+	public static void clickOnBack(AndroidDriver<AndroidElement> driver,int noOfTime) throws InterruptedException{
+		for(int i=0;i<noOfTime;i++){
+		driver.pressKeyCode(AndroidKeyCode.BACK);
+		Thread.sleep(500);
+		}
+	}
+}
